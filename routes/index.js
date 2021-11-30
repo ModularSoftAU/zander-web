befb1626a405e0ddefd16d99deaf4aae7a0fb889
shadowolf <shadowolfbusiness@gmail.com>
2021-12-01 07:31:40 +1100

    Commit dummy home page.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const config = require('../config.json');
 
 router.get('/', (req, res, next) => {
     res.render('index', {
-        "pagetitle": "Index",
+        "pageTitle": `Home`,
+        config: config
     });
 });
 
