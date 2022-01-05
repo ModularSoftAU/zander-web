c1551530347f7dc0acb933391335e1cf645f1857
shadowolf <shadowolfbusiness@gmail.com>
2022-01-05 21:51:48 +1100

    Refactor miniHeader and missing reply.render

--- routes/policyRoutes.js
+++ routes/policyRoutes.js
@@ -1,28 +1,28 @@
 export default function policySiteRoute(app, config) {
 
     app.get('/terms', async function(request, reply) {
-        reply.render('policy/termsOfService', {
+        reply.view('policy/termsOfService', {
             "pageTitle": `Network Terms Of Service`,
             config: config
         });
     });
 
     app.get('/rules', async function(request, reply) {
-        reply.render('policy/rules', {
+        reply.view('policy/rules', {
             "pageTitle": `Network Rules`,
             config: config
         });
     });
 
     app.get('/privacy', async function(request, reply) {
-        reply.render('policy/privacy', {
+        reply.view('policy/privacy', {
             "pageTitle": `Network Privacy Policy`,
             config: config
         });
     });
 
     app.get('/refund', async function(request, reply) {
-        reply.render('policy/refund', {
+        reply.view('policy/refund', {
             "pageTitle": `Network Refund Policy`,
             config: config
         });
