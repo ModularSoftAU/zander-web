d6327c3fd71baa288ea12778ac26b701641c5b29
shadowolf <shadowolfbusiness@gmail.com>
2022-03-24 15:27:28 +1100

    Implemented Application editor and routes.

--- routes/dashboard/index.js
+++ routes/dashboard/index.js
@@ -3,6 +3,7 @@ import dashboardEventSiteRoute from './events'
 import dashboardKnowledgebaseSiteRoute from './knowledgebase'
 import dashboardRanksSiteRoute from './ranks'
 import dashboardServersSiteRoute from './servers'
+import dashboardApplicationsSiteRoute from './applications'
 
 export default function dashbordSiteRoutes(app, fetch, moment, config, db) {
   
@@ -11,5 +12,6 @@ export default function dashbordSiteRoutes(app, fetch, moment, config, db) {
     dashboardKnowledgebaseSiteRoute(app, fetch, moment, config, db);
     dashboardRanksSiteRoute(app, fetch, config);
     dashboardServersSiteRoute(app, fetch, config, db);
+    dashboardApplicationsSiteRoute(app, fetch, config, db);
 
 }
