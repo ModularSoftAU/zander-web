e1986be2dd961f756c38d05d310a6da8bf304be8
shadowolf <shadowolfbusiness@gmail.com>
2022-03-25 20:25:04 +1100

    Revert "Gut and remove Discord commands and events. (#33)"
    This reverts commit c870779ed7080826f2ded06c961e0d62a13a8b85.

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 import packageData from './package.json';
+import DiscordJS from 'discord.js'
+import WOKCommands from 'wokcommands'
 import moment from 'moment'
 import fetch from 'node-fetch'
 
@@ -8,6 +10,68 @@ import fastifyCookie from 'fastify-cookie'
 
 import config from './config.json';
 import db from './controllers/databaseController'
+
+// Paths
+import path from 'path'
+import { fileURLToPath } from 'url'
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// 
+// Discord Related
+// 
+const { Intents } = DiscordJS
+
+const DiscordClient = new DiscordJS.Client({
+    // These intents are recommended for the built in help menu
+    intents: [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MEMBERS,
+        Intents.FLAGS.GUILD_MESSAGES,
+        Intents.FLAGS.DIRECT_MESSAGES,
+        Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+    ],
+})
+
+DiscordClient.on('ready', () => {
+    new WOKCommands(DiscordClient, {
+            // The name of the local folder for your command files
+            commandsDir: path.join(__dirname, 'discord/commands'),
+
+            // The name of the local folder for your feature files
+            featuresDir: path.join(__dirname, 'discord/features'),
+
+            // If WOKCommands warning should be shown or not, default true
+            showWarns: false,
+
+            // How many seconds to keep error messages before deleting them
+            // -1 means do not delete, defaults to -1
+            delErrMsgCooldown: -1,
+
+            // If your commands should not be ran by a bot, default false
+            ignoreBots: true,
+
+            // If interactions should only be shown to the one user
+            // Only used for when WOKCommands sends an interaction response
+            // Default is true
+            ephemeral: true,
+
+            typeScript: true,
+
+            // What server/guild IDs are used for testing only commands & features
+            // Can be a single string if there is only 1 ID
+            testServers: ['899441191416901632', '581056239312568332'],
+
+            // User your own ID
+            // If you only have 1 ID then you can pass in a string instead
+            botOwners: ['169978063478587392'],
+
+            // Provides additional debug logging
+            debug: false
+        })
+})
+
+DiscordClient.login(config.discord.apiKey);
 
 // 
 // Website Related
