a52e6b063e9675ed2070dd8c638a94b952701825
shadowolf <shadowolfbusiness@gmail.com>
2022-03-26 08:33:37 +1100

    Added punishment.

--- api/routes/punishment.js
+++ api/routes/punishment.js
@@ -1,7 +1,14 @@
-export default function punishmentApiRoute(app, config, db) {
+export default function punishmentApiRoute(app, config, db, features, lang) {
     const baseEndpoint = config.siteConfiguration.apiRoute + '/punishment';
 
     app.post(baseEndpoint + '/issue', async function(req, res) {
+        if (features.punishments == false) {
+            return res.send({
+                success: false,
+                message: `${lang.api.featureDisabled}`
+            });
+        }
+
         const playerUsername = req.body.playerUsername;
         const staffUsername = req.body.staffUsername;
         const platform = req.body.platform;
@@ -14,6 +21,13 @@ export default function punishmentApiRoute(app, config, db) {
     });
 
     app.post(baseEndpoint + '/delete', async function(req, res) {
+        if (features.punishments == false) {
+            return res.send({
+                success: false,
+                message: `${lang.api.featureDisabled}`
+            });
+        }
+
         const punishmentId = req.body.punishmentId;
 
         // ...
@@ -21,6 +35,13 @@ export default function punishmentApiRoute(app, config, db) {
     });
 
     app.get(baseEndpoint + '/user', async function(req, res) {
+        if (features.punishments == false) {
+            return res.send({
+                success: false,
+                message: `${lang.api.featureDisabled}`
+            });
+        }
+
         const username = req.query.username;
 
         // ...
@@ -28,6 +49,13 @@ export default function punishmentApiRoute(app, config, db) {
     });
 
     app.post(baseEndpoint + '/latest', async function(req, res) {
+        if (features.punishments == false) {
+            return res.send({
+                success: false,
+                message: `${lang.api.featureDisabled}`
+            });
+        }
+        
         const latest = req.body.latest;
 
         // ...
