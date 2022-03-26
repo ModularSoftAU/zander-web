bfb5cb260ceb1fe9d4fa542bef54ddc6d063b1ce
shadowolf <shadowolfbusiness@gmail.com>
2022-03-26 20:57:29 +1100

    Added CC routes and finalised layout.

--- api/routes/communitycreation.js
+++ api/routes/communitycreation.js
@@ -2,8 +2,173 @@ export default function communityCreationApiRoute(app, config, db) {
     const baseEndpoint = config.siteConfiguration.apiRoute + '/communitycreation';
 
     app.get(baseEndpoint + '/get', async function(req, res) {
-        // ...
-        res.send({ success: true });
+        // Note: One or more of these could be null.
+        const username = req.query.username;
+        const creationId = req.query.id;
+		const approvedOnly = req.query.approvedOnly;
+
+		// Whether or not to /get unapproved creations
+		var approved = '0';
+		if (approvedOnly === false) {
+			approved = '0,1';
+		}
+
+		// If the ?username= is used, get all creations by that user
+		if (username) {
+			try {
+				db.query(`
+					SELECT
+						cc.*,
+						imagesUnapproved.count AS totalImagesUnapproved,
+						cci.imageLink AS coverImageLink,
+						u.username
+					FROM communityCreations cc
+						JOIN users u ON cc.creatorId = u.userId
+						LEFT JOIN communityCreationImages cci ON cc.creationId = cci.creationId
+							AND cci.cover = 1
+							AND cci.approved IN (?)
+						LEFT JOIN (
+							SELECT
+								creationId,
+								COUNT(*) AS count
+							FROM communityCreationImages
+							WHERE approved = 0
+							GROUP BY creationId
+						) imagesUnapproved ON cc.creationId = imagesUnapproved.creationId
+					WHERE u.username = ?
+						AND cc.approved IN (?)
+				`, [approved, username, approved], function(error, results, fields) {
+					if (error) {
+						return res.send({
+							success: false,
+							message: `${error}`
+						});
+					}
+                    
+                    if (!results.length) {
+                        return res.send({
+                            success: false,
+                            message: `There are no results`
+                        });
+                    }
+                    
+					return res.send({
+						success: true,
+						data: results
+					});
+				});
+			} catch (error) {
+				res.send({
+					success: false,
+					message: `${error}`
+				});
+			}
+		}
+
+		// If the ?id= is used, get specific creation with all images
+		if (creationId) {
+			try {
+				db.query(`
+					SELECT
+						cc.*,
+						imagesUnapproved.count AS totalImagesUnapproved,
+						cci.imageLink,
+						cci.cover,
+						cci.position,
+						cci.approved AS imageApproved,
+						u.username
+					FROM communityCreations cc
+						JOIN users u ON cc.creatorId = u.userId
+						LEFT JOIN communityCreationImages cci ON cc.creationId = cci.creationId
+							AND cci.approved IN (?)
+						LEFT JOIN (
+							SELECT
+								creationId,
+								COUNT(*) AS count
+							FROM communityCreationImages
+							WHERE approved = 0
+							GROUP BY creationId
+						) imagesUnapproved ON cc.creationId = imagesUnapproved.creationId
+					WHERE cc.creationId = ?
+						AND cc.approved IN (?)
+					ORDER BY cci.position ASC
+				`, [approved, creationId, approved], function(error, results, fields) {
+					if (error) {
+						return res.send({
+							success: false,
+							message: `${error}`
+						});
+					}
+
+                    if (!results.length) {
+                        return res.send({
+                            success: false,
+                            message: `There are no results`
+                        });
+                    }
+
+					return res.send({
+						success: true,
+						data: results
+					});
+				});
+			} catch (error) {
+				res.send({
+					success: false,
+					message: `${error}`
+				});
+			}
+		}
+
+		// No username or id parameter, pull full list
+        try {
+            db.query(`
+				SELECT
+					cc.*,
+					imagesUnapproved.count AS totalImagesUnapproved,
+					cci.imageLink AS coverImageLink,
+					u.username
+				FROM communityCreations cc
+					JOIN users u ON cc.creatorId = u.userId
+					LEFT JOIN communityCreationImages cci ON cc.creationId = cci.creationId
+						AND cci.cover = 1
+						AND cci.approved IN (?)
+					LEFT JOIN (
+						SELECT
+							creationId,
+							COUNT(*) AS count
+						FROM communityCreationImages
+						WHERE approved = 0
+						GROUP BY creationId
+					) imagesUnapproved ON cc.creationId = imagesUnapproved.creationId
+				WHERE cc.approved IN (?)
+			`, [approved, approved], function(error, results, fields) {
+                if (error) {
+                    return res.send({
+                        success: false,
+                        message: `${error}`
+                    });
+                }
+
+                if (!results.length) {
+                    return res.send({
+                        success: false,
+                        message: `There are no results`
+                    });
+                }
+
+                return res.send({
+                    success: true,
+                    data: results
+                });
+            });
+
+        } catch (error) {
+            res.send({
+                success: false,
+                message: `${error}`
+            });
+        }
     });
 
     app.post(baseEndpoint + '/submit', async function(req, res) {
@@ -26,8 +191,10 @@ export default function communityCreationApiRoute(app, config, db) {
                     return element !== '';
                 });
 
-                // Loop through all filtered images and insert them into the communityCreationImages table
-                creationImageFiltered.forEach(function (image) {
+                console.log(creationImageFiltered);
+                console.log(creationInsertResults.insertId);
+
+                creationImageFiltered.forEach(image => {
                     db.query(`INSERT INTO communityCreationImages (creationId, imageLink) VALUES (?, ?)`, [creationInsertResults.insertId, image], function(error, results, fields) {
                         if (error) {
                             return res.send({
@@ -35,8 +202,8 @@ export default function communityCreationApiRoute(app, config, db) {
                                 message: `${error}`
                             });
                         }
-                    });
-                })
+                    });                    
+                });
             });
 
         } catch (error) {
@@ -46,8 +213,22 @@ export default function communityCreationApiRoute(app, config, db) {
             });
         }
     });
+    
+    app.post(baseEndpoint + '/approve', async function(req, res) {
+        const creationId = req.body.creationId;
+
+        // ...
+        res.send({ success: true });
+    });
 
     app.post(baseEndpoint + '/delete', async function(req, res) {
+        const creationId = req.body.creationId;
+
+        // ...
+        res.send({ success: true });
+    });
+
+    app.post(baseEndpoint + '/like', async function(req, res) {
         const creationId = req.body.creationId;
 
         // ...
--- routes/communityCreationsRoutes.js
+++ routes/communityCreationsRoutes.js
@@ -4,10 +4,18 @@ export default function communityCreationSiteRoute(app, fetch, moment, config) {
     // Community Creations
     // 
     app.get('/communityCreations', async function(request, reply) {
+        const fetchURL = `${config.siteConfiguration.siteAddress}${config.siteConfiguration.apiRoute}/communitycreation/get`;
+        const response = await fetch(fetchURL);
+        const apiData = await response.json();
+
+        console.log(apiData);
+
         return reply.view('modules/communityCreation/communityCreation', {
             "pageTitle": `Community Creations`,
             config: config,
-            request: request
+            request: request,
+            moment: moment,
+            apiData: apiData
         });
     });
 
