03c5243c83c175583602608bf27fefdac4699418
shadowolf <shadowolfbusiness@gmail.com>
2022-03-26 16:31:20 +1100

    Implemented CC submit with images and form.

--- api/routes/communitycreation.js
+++ api/routes/communitycreation.js
@@ -7,22 +7,36 @@ export default function communityCreationApiRoute(app, config, db) {
     });
 
     app.post(baseEndpoint + '/submit', async function(req, res) {
-        const creator = req.body.creator;
+        const creatorId = req.body.creatorId;
         const creationName = req.body.creationName;
         const creationDescription = req.body.creationDescription;
+        const creationImage = req.body.creationImage;
 
         try {
-            db.query(`INSERT INTO communityCreations (creatorId, creationName, creationDescription) VALUES ((select userId from users where username=?), ?, ?)`, [creator, creationName, creationDescription], function(error, results, fields) {
+            db.query(`INSERT INTO communityCreations (creatorId, creationName, creationDescription) VALUES (?, ?, ?)`, [creatorId, creationName, creationDescription], function(error, creationInsertResults, fields) {
                 if (error) {
                     return res.send({
                         success: false,
                         message: `${error}`
                     });
                 }
-                return res.send({
-                    success: true,
-                    message: `The creation ${creationName} has been successfully submitted for approval.`
+
+                // Filter out empty image entries.
+                const creationImageFiltered = creationImage.filter(element => {
+                    return element !== '';
                 });
+
+                // Loop through all filtered images and insert them into the communityCreationImages table
+                creationImageFiltered.forEach(function (image) {
+                    db.query(`INSERT INTO communityCreationImages (creationId, imageLink) VALUES (?, ?)`, [creationInsertResults.insertId, image], function(error, results, fields) {
+                        if (error) {
+                            return res.send({
+                                success: false,
+                                message: `${error}`
+                            });
+                        }
+                    });
+                })
             });
 
         } catch (error) {
