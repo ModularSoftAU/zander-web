edf62afa4ba59770dbaba5260eebc7e90e136570
Aron Brown <webmaster@birdwingfx.com>
2022-05-08 08:37:57 -0400

    Update session.js
    Add the /api/session/destroy  and /api/session/switch  routes

--- api/routes/session.js
+++ api/routes/session.js
@@ -48,38 +48,37 @@ export default function sessionApiRoute(app, config, db, features, lang) {
         isFeatureEnabled(features.sessions, res, lang);
         const uuid = required(req.body, "uuid", res);
 
-        // try {
-        //     // Insert newly started session into database
-        //     db.query(`
-        //         UPDATE gameSessions 
-        //             (
-        //                 userId, 
-        //                 ipAddress, 
-        //                 serverId
-        //             ) VALUES (
-        //                 (SELECT userId FROM users WHERE uuid=?), 
-        //                 ?,
-        //                 (SELECT serverId FROM servers WHERE name=?)
-        //             )`, [uuid, ipAddress, server], function(error, results, fields) {
-        //         if (error) {
-        //             return res.send({
-        //                 success: false,
-        //                 message: `${error}`
-        //             });
-        //         }
+        try {
+            // Update any open sessions for the specified user to close them
+			// The 'AND gameSessions.sessionId > 0' line is necessary to bypass mySQL's "safe update" feature.
+			// If this is not there and there is somehow more than 1 open session, the query will fail.
+            db.query(`
+                UPDATE gameSessions, users
+					SET gameSessions.sessionEnd = NOW()
+				WHERE gameSessions.userId = users.userId
+					AND users.uuid = ?
+					AND gameSessions.sessionEnd IS NULL
+					AND gameSessions.sessionId > 0
+				`, [uuid], function(error, results, fields) {
+                if (error) {
+                    return res.send({
+                        success: false,
+                        message: `${error}`
+                    });
+                }
 
-        //         return res.send({
-        //             success: true,
-        //             message: `New session for ${uuid} has been created.`
-        //         });
-        //     });
+                return res.send({
+                    success: true,
+                    message: `All sessions for ${uuid} has been closed.`
+                });
+            });
 
-        // } catch (error) {
-        //     res.send({
-        //         success: false,
-        //         message: `${error}`
-        //     });
-        // }
+        } catch (error) {
+            res.send({
+                success: false,
+                message: `${error}`
+            });
+        }
 
     });
 
@@ -88,8 +87,38 @@ export default function sessionApiRoute(app, config, db, features, lang) {
         const uuid = required(req.body, "uuid", res);
         const server = required(req.body, "server", res);
 
-        // ...
-        res.send({ success: true });
+        try {
+            // Update any open sessions for the specified user to change to the specified server
+			// The 'AND gameSessions.sessionId > 0' line is necessary to bypass mySQL's "safe update" feature.
+			// If this is not there and there is somehow more than 1 open session, the query will fail.
+            db.query(`
+                UPDATE gameSessions, users, servers
+					SET gameSessions.serverId = servers.serverId
+				WHERE gameSessions.userId = users.userId
+					AND users.uuid = ?
+					AND gameSessions.sessionEnd IS NULL
+					AND servers.name = ?
+					AND gameSessions.sessionId > 0
+				`, [uuid, server], function(error, results, fields) {
+                if (error) {
+                    return res.send({
+                        success: false,
+                        message: `${error}`
+                    });
+                }
+
+                return res.send({
+                    success: true,
+                    message: `${uuid} has switched server to ${server}.`
+                });
+            });
+
+        } catch (error) {
+            res.send({
+                success: false,
+                message: `${error}`
+            });
+        }
     });
 
 }
