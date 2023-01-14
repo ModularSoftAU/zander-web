7b31ec988c086e91d146dc27d1d9085aee9f0e32
benrobson <shadowolfbusiness@gmail.com>
2023-01-15 09:43:33 +1100

    Add current year to Copyright

--- api/routes/announcement.js
+++ api/routes/announcement.js
@@ -19,7 +19,6 @@ export default function announcementApiRoute(app, config, db, features, lang) {
                         });
                     }
 
-
                     if (!results.length) {
                         return res.send({
                             success: false,
