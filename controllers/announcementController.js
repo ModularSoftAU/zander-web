85cd88970eac7a23613b5d6716f1fde886357852
benrobson <shadowolfbusiness@gmail.com>
2023-10-18 08:11:09 +1100

    Remove example files in favour of docs, and resolve issue with getAnnouncement.

--- controllers/announcementController.js
+++ controllers/announcementController.js
@@ -5,9 +5,27 @@ import fetch from "node-fetch";
 */
 export async function getWebAnnouncement() {
     const fetchURL = `${process.env.siteAddress}/api/announcement/get?announcementType=web`;
-    const response = await fetch(fetchURL, {
-        headers: { 'x-access-token': process.env.apiKey }
-    });
-    const apiData = await response.json();
-    return apiData.data[0];
+    try {
+        const response = await fetch(fetchURL, {
+            headers: { 'x-access-token': process.env.apiKey }
+        });
+
+        if (!response.ok) {
+            // Handle the case where the API request is not successful, e.g., non-2xx status code.
+            throw new Error(`Failed to fetch announcement data. Status: ${response.status}`);
+        }
+
+        const apiData = await response.json();
+
+        if (apiData.data && apiData.data.length > 0) {
+            return apiData.data[0];
+        } else {
+            // Handle the case where there are no announcements.
+            return null; // or return a default value as needed
+        }
+    } catch (error) {
+        // Handle other potential errors such as network issues or JSON parsing errors.
+        console.error("Error fetching announcement:", error);
+        return null; // or throw an error, or handle it differently as needed
+    }
 }
