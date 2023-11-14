9461d6db60c8be7da27cbcbaeb85ceb01da2bf9e
benrobson <shadowolfbusiness@gmail.com>
2023-11-14 19:12:00 +1100

    Changed from HEX to Colour

--- listeners/guildMemberBoostUpdate.js
+++ listeners/guildMemberBoostUpdate.js
@@ -1,6 +1,6 @@
 import { Listener } from '@sapphire/framework';
 import config from '../config.json' assert {type: "json"};
-import { EmbedBuilder } from 'discord.js';
+import { Colors, EmbedBuilder } from 'discord.js';
 import features from '../features.json' assert {type: "json"};
 
 export class GuildMemberBoostUpdateListener extends Listener {
@@ -25,8 +25,8 @@ export class GuildMemberBoostUpdateListener extends Listener {
 
       if (!oldStatus && newStatus) {
         let embed = new EmbedBuilder()
-          .setTitle(`${newMember.user.username} has boosted the Server!  :tada:`)
-          .setColor(`#f47fff`)
+          .setTitle(`\`${newMember.user.username}\` has boosted the Server! :tada:`)
+          .setColor(Colors.DarkVividPink)
         welcomechannel.send({embeds: [embed]});
       }
       return;
