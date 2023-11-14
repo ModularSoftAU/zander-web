e6e86b3eeb4c8f5316ebcea1dd6e6b87096fba57
benrobson <shadowolfbusiness@gmail.com>
2023-11-14 18:48:15 +1100

    Fixed issue where filter errors after response is sent.

--- api/routes/filter.js
+++ api/routes/filter.js
@@ -1,71 +1,73 @@
-import {expandString, isFeatureEnabled, required} from '../common';
-import filter from '../../filter.json' assert {type: "json"};
+import { expandString, isFeatureEnabled, required } from '../common';
+import filter from '../../filter.json' assert { type: "json" };
 
 export default function filterApiRoute(app, config, db, features, lang) {
     const baseEndpoint = '/api/filter';
 
-    app.post(baseEndpoint, async function(req, res) {
+    app.post(baseEndpoint, async function (req, res) {
         function expandString(string, filter) {
             var regexString = "";
             for (var i = 0; i < string.length; i++) {
-                // If the character does not have any aliases then just
-                // use the character. Note, this is a regex character.
                 if (string[i] in filter.alias)
                     regexString += "[" + filter.alias[string[i]] + "]";
                 else
                     regexString += string[i]
             }
-            regexString = regexString.replace(".", "\\.")
-            return regexString
+            regexString = regexString.replace(".", "\\.");
+            return regexString;
         }
-        
-        // Hack to show the error we expect when both are disabled
+
         if (!features.filter.phrase && !features.filter.link)
-            return isFeatureEnabled(false, res, lang)
-        
+            return isFeatureEnabled(false, res, lang);
+
         const content = required(req.body, "content", res);
 
-        var bannedWords = []
+        var bannedWords = [];
         if (features.filter.phrase)
-            bannedWords = bannedWords.concat(filter.phrases)
+            bannedWords = bannedWords.concat(filter.phrases);
         if (features.filter.link)
-            bannedWords = bannedWords.concat(filter.links)
-        
-        var bannedRegex = []
-        // Usually compiling regex on the fly like this isn't recommended.
-        // You can compile regex once and then reuse it. Since performance
-        // isn't a big deal and the word list is yet to expand, this will
-        // suffice for now.
+            bannedWords = bannedWords.concat(filter.links);
+
+        var bannedRegex = [];
         bannedWords.forEach(bannedWord => {
-            bannedRegex.push(new RegExp(expandString(bannedWord, filter)))
-        })
+            bannedRegex.push(new RegExp(expandString(bannedWord, filter)));
+        });
+
+        let responseSent = false;
 
         try {
             const wordList = content.split(" ");
-            bannedRegex.forEach(re => {
-                wordList.forEach(word => {
+            for (let i = 0; i < bannedRegex.length; i++) {
+                const re = bannedRegex[i];
+                for (let j = 0; j < wordList.length; j++) {
+                    const word = wordList[j];
                     if (re.test(word)) {
-                        return res.send({
+                        res.send({
                             success: false,
                             message: lang.filter.phraseCaught
                         });
+                        responseSent = true;
+                        break;
                     }
+                }
+                if (responseSent) {
+                    break;
+                }
+            }
+
+            if (!responseSent) {
+                res.send({
+                    success: true,
+                    message: `Content Clean`
                 });
-            });
-            return res.send({
-                success: true,
-                message: `Content Clean`
-            });
+            }
         } catch (error) {
             console.log(error);
 
-            return res.send({
+            res.send({
                 success: false,
                 message: lang.web.registrationError
             });
         }
-
-        return res;
     });
-
 }
