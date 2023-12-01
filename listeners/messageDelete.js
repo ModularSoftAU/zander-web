1383da3e6b532c209d8514d941d50127b400cede
benrobson <shadowolfbusiness@gmail.com>
2023-12-01 20:49:47 +1100

    Fix message delete and update not going to admin log.

--- listeners/messageDelete.js
+++ listeners/messageDelete.js
@@ -1,6 +1,7 @@
 import { Listener } from "@sapphire/framework";
 import config from "../config.json" assert { type: "json" };
 import { Colors, EmbedBuilder } from "discord.js";
+import { MessageBuilder } from "discord-webhook-node";
 
 export class GuildMessageDeleteListener extends Listener {
   constructor(context, options) {
@@ -15,12 +16,11 @@ export class GuildMessageDeleteListener extends Listener {
     // Check if the author of the deleted message is a bot and stop if true.
     if (message.author.bot) return;
 
-    let adminLogChannel = message.guild.channels.cache.find(
-      (c) => c.id === config.discord.channels.adminLog
+    const adminLogHook = new Webhook(
+      config.discord.webhooks.adminLog
     );
-    if (!adminLogChannel) return;
 
-    const embed = new EmbedBuilder()
+    const embed = new MessageBuilder()
       .setTitle("Message Delete")
       .setColor(Colors.Red)
       .setDescription(
@@ -32,6 +32,6 @@ export class GuildMessageDeleteListener extends Listener {
         inline: false,
       });
 
-    adminLogChannel.send({ embeds: [embed] });
+    adminLogHook.send(embed);
   }
 }
--- listeners/messageUpdate.js
+++ listeners/messageUpdate.js
@@ -1,6 +1,7 @@
 import { Listener } from "@sapphire/framework";
 import config from "../config.json" assert { type: "json" };
 import { Colors, EmbedBuilder } from "discord.js";
+import { MessageBuilder } from "discord-webhook-node";
 
 export class GuildMessageUpdateListener extends Listener {
   constructor(context, options) {
@@ -15,12 +16,11 @@ export class GuildMessageUpdateListener extends Listener {
     // Check if the author is a bot and stop if true.
     if (newMessage.author.bot) return;
 
-    let adminLogChannel = oldMessage.guild.channels.cache.find(
-      (c) => c.id === config.discord.channels.adminLog
+    const adminLogHook = new Webhook(
+      config.discord.webhooks.adminLog
     );
-    if (!adminLogChannel) return;
 
-    const embed = new EmbedBuilder()
+    const embed = new MessageBuilder()
       .setTitle("Message Edit")
       .setColor(Colors.Yellow)
       .setDescription(
@@ -35,6 +35,6 @@ export class GuildMessageUpdateListener extends Listener {
         }
       );
 
-    adminLogChannel.send({ embeds: [embed] });
+    adminLogHook.send(embed);
   }
 }
