504c4902ac74c66c12875ac390dfd89fa7f173a5
benrobson <shadowolfbusiness@gmail.com>
2024-02-21 14:52:05 +1100

    Added discord voice and message audit events.

--- controllers/auditController.js
+++ controllers/auditController.js
@@ -45,3 +45,33 @@ export async function updateAudit_lastWebsiteLogin(auditDateTime, username) {
     }
   );
 }
+
+export async function updateAudit_lastDiscordMessage(auditDateTime, discordId) {
+  const userData = new UserGetter();
+  const userAudit = await userData.byDiscordId(discordId);
+
+  db.query(
+    `UPDATE users SET audit_lastDiscordMessage=? WHERE userId=?;`,
+    [auditDateTime, userAudit.userId],
+    function (error, results, fields) {
+      if (error) {
+        reject(error);
+      }
+    }
+  );
+}
+
+export async function updateAudit_lastDiscordVoice(auditDateTime, discordId) {
+  const userData = new UserGetter();
+  const userAudit = await userData.byDiscordId(discordId);
+
+  db.query(
+    `UPDATE users SET audit_lastDiscordVoice=? WHERE userId=?;`,
+    [auditDateTime, userAudit.userId],
+    function (error, results, fields) {
+      if (error) {
+        reject(error);
+      }
+    }
+  );
+}
