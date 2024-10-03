2393c0c03be5ec4d53c7c65ea55ca6ab1a784ca2
benrobson <shadowolfbusiness@gmail.com>
2024-10-03 21:54:32 +1000

    Add vault get/add/update/delete

--- api/internal_redirect/index.js
+++ api/internal_redirect/index.js
@@ -3,6 +3,7 @@ import serverRedirectRoute from "./server";
 import webRedirectRoute from "./web";
 import announcementsRedirectRoute from "./announcement";
 import reportRedirectRoute from "./report";
+import vaultRedirectRoute from "./vault";
 
 export default (app, config, lang) => {
   applicationRedirectRoute(app, config, lang);
@@ -10,4 +11,5 @@ export default (app, config, lang) => {
   reportRedirectRoute(app, config, lang);
   webRedirectRoute(app, config, lang);
   announcementsRedirectRoute(app, config, lang);
+  vaultRedirectRoute(app, config, lang);
 };
