642025af414424405f9e9dba8740e282a2ef39f7
Ben Robson <shadowolfbusiness@gmail.com>
2026-07-25 13:06:26 +1000

    test: add JUnit 5 to zander-hub and zander-velocity

---
+++ zander-hub/src/test/java/dev/anchorlight/zander/hub/SmokeTest.java
@@ -0,0 +1,11 @@
+package dev.anchorlight.zander.hub;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SmokeTest {
+    @Test
+    void junitIsWired() {
+        assertEquals(2, 1 + 1);
+    }
+}
---
+++ zander-velocity/src/test/java/dev/anchorlight/zander/velocity/SmokeTest.java
@@ -0,0 +1,11 @@
+package dev.anchorlight.zander.velocity;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SmokeTest {
+    @Test
+    void junitIsWired() {
+        assertEquals(2, 1 + 1);
+    }
+}
